55a0a34342fcbffd2aa1cbdc74a559110fdd0e6f
anthonyobispo <114023964+anthonyobispo@users.noreply.github.com>
2022-11-01 20:08:10 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const morgan = require('morgan');
 const app = express();
+
+//app.use(express.json());
+//get all restaurants
 app.use(cors());
-app.get("/nts", (req, res) => {
-    res.status(404).json({
+app.get("/api/v1/restaurants", (req, res,) => {
+ console.log("route handler ran");
+    res.status(200).json({
         status:"success",
-        restaurants:"michoacana",
+        data: {
+            restaurants:["michoacana", "7eleven"],
+        }
     })
 });
 //http://localhost:5000/getRestaurants
+
+//get a restaurant method
+app.get("/api/v1/restaurants/:restaurantid", (req,res) => {
+    console.log(req.params);
+})
+
+
+//create a restaurant
+app.post("api/v1/restaurants", (req,res)=> {
+    console.log(req.body);
+});
 
 
 const port = process.env.PORT || 5000;
